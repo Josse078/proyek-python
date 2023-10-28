c7e38812343c7d1fc690bd277644b3fcb129a999
Josse078 <139468237+Josse078@users.noreply.github.com>
2023-10-28 14:29:37 +0700

    Add files via upload
    implementasi bert ke chatbot

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import os
 import json
 from difflib import get_close_matches
+from transformers import BertTokenizer, BertForQuestionAnswering
+import torch
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+model = BertForQuestionAnswering.from_pretrained("bert-base-uncased")
 def load_knowledge_base(file_path:str) -> dict:
     with open(file_path,'r') as file:
         data : dict = json.load(file)
@@ -16,26 +20,59 @@ def get_answer_for(question: str,knowledge_base:dict) -> str | None:
     for q in knowledge_base["questions"]:
         if q["question"] == question:
             return q["answer"]
+
+# def chat_bot():
+#     knowledge_base: dict = load_knowledge_base('/home/josse/informatika/proyek python/output.json')
+#     while True:
+#         user_input: str = input('You :')
+
+#         if user_input.lower() == 'quit':
+#             break
+        
+#         best_match:str | None = find_best_match(user_input,[q["question"]for q in knowledge_base["questions"]])
+
+#         if best_match:
+#             answer: str = get_answer_for(best_match,knowledge_base)
+#             print(f'Bot:{answer}')
+#         else:
+#             print('Bot: I don\'t know the answer.Can you teach me?')
+#             new_answer: str = input('Type the answer of "skip" to skip')
+#             if new_answer.lower() != 'skip':
+#                 knowledge_base["questions"].append({"question": user_input,"answer":new_answer})
+#                 save_knowledge_base('knowledge_base.json',knowledge_base)
+#                 print('Bot: Thank you! I learned a new response')
+def answer_question(question, context):
+    inputs = tokenizer(question, context, return_tensors="pt")
+    start_scores, end_scores = model(**inputs).values() 
+
+    start_index = torch.argmax(start_scores)
+    end_index = torch.argmax(end_scores)
+
+    
+    start_index = int(start_index)
+    end_index = int(end_index)
+
+    input_ids = inputs["input_ids"][0].tolist()
+    answer = tokenizer.decode(input_ids[start_index:end_index + 1])
+
+    return answer
+
+import json
+
 def chat_bot():
-    knowledge_base: dict = load_knowledge_base('/home/josse/informatika/proyek python/output.json')
+    
+    with open('/home/josse/informatika/proyek python/context.json', 'r') as file:
+        context_data = json.load(file)
+        context = context_data.get("context", "")
+
     while True:
-        user_input: str = input('You :')
+        user_input = input('You: ')
 
         if user_input.lower() == 'quit':
             break
-        
-        best_match:str | None = find_best_match(user_input,[q["question"]for q in knowledge_base["questions"]])
 
-        if best_match:
-            answer: str = get_answer_for(best_match,knowledge_base)
-            print(f'Bot:{answer}')
-        else:
-            print('Bot: I don\'t know the answer.Can you teach me?')
-            new_answer: str = input('Type the answer of "skip" to skip')
-            if new_answer.lower() != 'skip':
-                knowledge_base["questions"].append({"question": user_input,"answer":new_answer})
-                save_knowledge_base('knowledge_base.json',knowledge_base)
-                print('Bot: Thank you! I learned a new response')
-            
+        answer = answer_question(user_input, context)
+        print(f'Bot: {answer}')
+
 if __name__ == '__main__':
     chat_bot()
