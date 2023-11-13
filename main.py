306efce30f9654364e2f2640b7d6c288ef51c1d4
Josse078 <139468237+Josse078@users.noreply.github.com>
2023-11-13 18:27:20 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-
 import os
 import json
 from difflib import get_close_matches
-from transformers import BertTokenizer, BertForQuestionAnswering
+from transformers import BertTokenizer, BertForQuestionAnswering, GPT2LMHeadModel, GPT2Tokenizer
 import torch
 import csv
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -14,19 +13,30 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import textwrap
+from transformers import AdamW
 
+gpt2_model = GPT2LMHeadModel.from_pretrained("gpt2")
+gpt2_tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 tokenizer = BertTokenizer.from_pretrained("indobenchmark/indobert-base-p1")
 model = BertForQuestionAnswering.from_pretrained("indobenchmark/indobert-base-p1")
 sentence_model = SentenceTransformer('bert-base-nli-mean-tokens')
 nlp = spacy.load("en_core_web_sm")
+def generate_answer_gpt2(question):
+    input_ids = gpt2_tokenizer.encode(question, return_tensors="pt")
+    output = gpt2_model.generate(input_ids, max_length=100, num_beams=5, no_repeat_ngram_size=2, top_k=50, top_p=0.95, temperature=0.7)
+    answer = gpt2_tokenizer.decode(output[0], skip_special_tokens=True)
+    return answer
+
 def preprocess_text(text):
     doc = nlp(text)
     tokens = [token.lemma_ for token in doc if not token.is_stop]
     return " ".join(tokens)
+
 def load_knowledge_base(file_path:str) -> dict:
-    with open(file_path,'r') as file:
-        data : dict = json.load(file)
+    with open(file_path, 'r') as file:
+        data: dict = json.load(file)
     return data
+
 def find_similar_question(user_question, dataset):
     vectorizer = TfidfVectorizer()
     question_vectors = vectorizer.fit_transform([user_question])
@@ -39,13 +49,16 @@ def find_similar_question(user_question, dataset):
             max_similarity = similarity
             most_similar_question = entry['question']
     return most_similar_question
-def save_knowledge_base(file_path:str,data:dict):
-    with open(file_path,'w') as file: 
-        json.dump(data,file,indent=2)
-def find_best_match(user_question:str,questions:list[str]) -> str | None:
-    matches:list = get_close_matches(user_question,questions,n=1,cutoff = 0.6)
+
+def save_knowledge_base(file_path:str, data:dict):
+    with open(file_path, 'w') as file: 
+        json.dump(data, file, indent=2)
+
+def find_best_match(user_question:str, questions:list[str]) -> str | None:
+    matches:list = get_close_matches(user_question, questions, n=1, cutoff=0.6)
     return matches[0] if matches else None
-def get_answer_for(question: str,knowledge_base:dict) -> str | None:
+
+def get_answer_for(question: str, knowledge_base:dict) -> str | None:
     for q in knowledge_base["questions"]:
         if q["question"] == question:
             return q["answer"]
@@ -68,7 +81,9 @@ def answer_question(question, dataset):
     if search_links:
         return f"I couldn't find a similar question in my knowledge base. Here are some relevant search results on Google:\n{', '.join(search_links)}"
     else:
-        return "I'm not sure how to answer that."
+        
+        gpt2_answer = generate_answer_gpt2(question)
+        return f"GPT-2 says: {gpt2_answer}"
 
 def load_qa_dataset(file_path):
     dataset = []
@@ -80,16 +95,30 @@ def load_qa_dataset(file_path):
             dataset.append({'question': question, 'answer': answer})
             dataset.append({'question': answer, 'answer': question})  
     return dataset
-#qa_dataset = load_qa_dataset('/home/josse/informatika/proyek python/databases/merged_file.csv')
-qa_dataset = load_qa_dataset('/home/josse/informatika/proyek python/databases/irrelevant datas/Conversation.csv')
-def chat_bot(dataset):
+
+def chat_bot(dataset, knowledge_base_path):
     while True:
         user_input = input('You: ')
         if user_input.lower() == 'quit':
             break
+
+       
         answer = answer_question(user_input, dataset)
+
+        
+        edit_response = input(f'Bot: {answer}\nIs this response accurate? (yes/no): ').lower()
+
+        if edit_response == 'no':
+            edited_response = input('Please provide the corrected response: ')
+           
+            dataset.append({'question': user_input, 'answer': edited_response})
+           
+            save_knowledge_base(knowledge_base_path, {'questions': dataset})
+            print('Thank you for the correction. The knowledge base has been updated.')
+
         print(f'Bot: {answer}')
+
 if __name__ == '__main__':
-    chat_bot(qa_dataset)
-
-
+    qa_dataset = load_qa_dataset('/home/josse/informatika/proyek python/databases/irrelevant datas/Conversation.csv')
+    knowledge_base_path = "knowledge_base.json"
+    chat_bot(qa_dataset, knowledge_base_path)
